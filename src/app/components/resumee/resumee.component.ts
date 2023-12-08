3562300a9899a46272fef1a252a81ea2d80a2caf
Gabriele Miano <gabriele.miano@docebo.com>
2023-12-08 17:31:51 +0100

    add resume

--- src/app/components/resumee/resumee.component.ts
+++ src/app/components/resumee/resumee.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {PdfViewerModule} from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-resumee',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PdfViewerModule],
   templateUrl: './resumee.component.html',
   styleUrl: './resumee.component.scss'
 })
 export class ResumeeComponent {
 
+  pathCvToDownload = '/assets/curriculum/en.pdf';
+  pathCvToPreview = '/assets/curriculum/en-dark.pdf';
+  printCv() {
+    window.open(this.pathCvToDownload, '_blank')
+  }
 }

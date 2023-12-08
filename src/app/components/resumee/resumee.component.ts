a1143b84d939f1e2db2cf9571ea0435f2eddaf37
Gabriele Miano <gabriele.miano@docebo.com>
2023-12-08 12:04:53 +0100

    webiste init

---
+++ src/app/components/resumee/resumee.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-resumee',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './resumee.component.html',
+  styleUrl: './resumee.component.scss'
+})
+export class ResumeeComponent {
+
+}

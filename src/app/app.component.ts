4aee304d84695620e9be0c569f456146e6f161ea
Gabriele Miano <gabriele.miano@docebo.com>
2023-12-09 22:51:49 +0100

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import {Component, HostListener, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
-import {MenuComponent} from './components/menu/menu.component';
-import {HamburgerMenuComponent} from './components/hamburger-menu/hamburger-menu.component';
+import {FullMenuComponent} from './components/menu/full-menu/full-menu.component';
+import {HamburgerMenuComponent} from './components/menu/hamburger-menu/hamburger-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuComponent, HamburgerMenuComponent],
+  imports: [CommonModule, RouterOutlet, FullMenuComponent, HamburgerMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
